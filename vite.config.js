137ab7bf2117e706363f80929b34b0caf264db0d
ヴ・アイン・キエト <vutrandangkhoa7@gmail.com>
2025-11-01 00:08:34 +0700

    .

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/',
+  build: { outDir: 'dist', assetsDir: 'assets', rollupOptions: { output: { manualChunks: undefined, }, }, },
+  server: { port: 3000, open: true, },
+  assetsInclude: ['**/*.jpg', '**/*.png', '**/*.mp3'],
+});
